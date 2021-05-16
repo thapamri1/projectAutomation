6a37e13dca1f717469c5c32ca092a042d8c6e803
Amrit Thapa <thapa.amrit101@gmail.com>
2021-05-16 07:29:17 -0700

    Added automated script except dates and final select

--- features/steps/orbitz.py
+++ features/steps/orbitz.py
@@ -1,5 +1,7 @@
 from behave import *
 from selenium import webdriver
+import time
+from datetime import date
 
 
 @given('launch chrome browser')
@@ -11,6 +13,67 @@ def launchBrowser(context):
 @then('Open the website')
 def checkOrbitzText(context):
     context.driver.get("https://www.orbitz.com/")
+
+
+@then('Select Flights')
+def selectFlight(context):
+    context.driver.find_element_by_link_text("Flights").click()
+
+
+@then('Select RoundTrip')
+def selectRoundTrip(context):
+    context.driver.implicitly_wait(5)
+    context.driver.find_element_by_link_text("Roundtrip").click()
+    # context.driver.find_element_by_id("Leaving from").click()
+
+
+@then('Click button')
+def clickButton(context):
+    context.driver.implicitly_wait(5)
+    context.driver.find_element_by_xpath("//button[@aria-label='Leaving from']").click()
+
+
+@then('Enter Leaving From "{leavingCity}" and Going to "{goingCity}" city')
+def leavingAndReturningCity(context, leavingCity, goingCity):
+    context.driver.find_element_by_xpath("//button[@aria-label='Leaving from']").send_keys(leavingCity)
+    context.driver.find_element_by_xpath(
+        "//strong[contains(text(),'San Francisco (SFO - San Francisco Intl.)')]").click()
+    context.driver.find_element_by_xpath("//button[@aria-label='Going to']").send_keys(goingCity)
+    context.driver.find_element_by_xpath("//strong[contains(text(), 'New York (JFK - John F. Kennedy Intl.)')]").click()
+    time.sleep(2)
+
+
+# @then('select select departing date and returning date')
+# def selectDates(context):
+#     context.driver.find_element_by_xpath("//button[@id='d1-btn']").click()
+#     time.sleep(2)
+#     # context.browser.find_element_by_css_selector("td.uitk-new-date-picker-day+td").click()
+#     today = date.today()
+#     print("Today's date:", today)
+#     time.sleep(4)
+
+
+@then('click Search Button')
+def clickSearchButton(context):
+    context.driver.find_element_by_xpath("//button[contains(text(),'Search')]").click()
+    time.sleep(5)
+
+
+@then('select most expensive flight')
+def selectMostExpFlight(context):
+    context.driver.find_element_by_xpath("//select[@id='sortDropdown']").click()
+    time.sleep(2)
+    context.driver.find_element_by_xpath("//option[contains(text(),'Price (Highest)')]").click()
+    time.sleep(8)
+    # context.driver.find_element_by_xpath("//input[@id='stopFilter_stops-0']").click()
+    # time.sleep()
+
+
+@then('Select Button')
+def selectFlight(context):
+    context.driver.find_element_by_xpath("//button[@type='button' and contains(., 'Select result 1 when sorted by "
+                                         "price')]").click()
+    time.sleep(10)
 
 
 @then('close browser')
--- features/test.feature
+++ features/test.feature
@@ -2,4 +2,11 @@ Feature: Check Orbitz website
   Scenario: go to orbitz website and close it
     Given launch chrome browser
     Then Open the website
+    Then Select Flights
+    Then Select RoundTrip
+    Then Enter Leaving From "San Francisco" and Going to "New York" city
+#    Then select select departing date and returning date
+    Then click Search Button
+    Then select most expensive flight
+    Then Select Button
     And close browser
